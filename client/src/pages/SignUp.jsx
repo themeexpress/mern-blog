fa647249696fd27d1b8050a72030331dc59c22ad
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2024-06-13 15:34:14 +0900

    Created Sign Up page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,47 @@
+import { Button, Label, TextInput } from 'flowbite-react';
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
-  return (
-    <div>SignUp</div>
-  )
+  return <div className='min-h-screen mt-20'>
+    <div className="flex p-3 mt-20 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
+      {/* left */}
+      <div className="flex-1">
+        <Link to="/" className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 
+            bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500
+            rounded-lg'>Kamrul's</span> Blog
+        </Link>
+        <p className='text-sm mt-5'>
+          This is full featured MERN tech demo project. You can signup with your email and pasword or with google account.
+        </p>
+      </div>
+      {/* right */}
+      <div className="flex-1">
+        <form className='flex flex-col gap-4'>
+          <div>
+            <Label value='Your username'></Label>
+            <TextInput 
+              type='text' placeholder='Username' id='username'></TextInput>
+          </div>
+          <div>
+            <Label value='Your email address'></Label>
+            <TextInput 
+              type='text' placeholder='name@company.com' id='email'></TextInput>
+          </div>
+          <div>
+            <Label value='Your password'></Label>
+            <TextInput 
+              type='text' placeholder='Password' id='password'></TextInput>
+          </div>
+          <Button gradientDuoTone='purpleToPink'>Sign Up</Button>
+        </form>
+        <div className="flex gap-2 text-sm mt-5">
+          <span> Have an account?</span>
+          <Link to='/sign-in' className='text-blue-500'>Sign In</Link>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 }
