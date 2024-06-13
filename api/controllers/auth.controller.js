d8bb512ee225673dd656f0d627c006b08a82c235
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2024-06-13 12:33:12 +0900

    completed middle ware and error handling functon

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,20 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils.js/error.js';
 
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
     const {username, email, password} = req.body;
 
-    if(!username || !email || !password || username === '' || email === '' || password === ''){
-        return res.status(400).json({ message: 'All fields are required' });
+    if(
+        !username || 
+        !email || 
+        !password || 
+        username === '' 
+        || email === '' || 
+        password === ''
+    ){
+        next(errorHandler(400, 'All fields are required'));
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -21,7 +29,7 @@ export const signUp = async (req, res) => {
         await newUser.save();
         res.json('Signup Successful');
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        next(error);
     }
 
 }
