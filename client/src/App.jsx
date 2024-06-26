911d9edff777ea3f9961af525c8a44c2766354d1
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2024-06-27 00:44:15 +0900

    Completed Footer UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp';
 import Projects from './pages/Projects';
 import Dashboard from './pages/Dashboard';
 import Header from './components/Header';
+import Footer from './components/Footer';
 export default function App() {
   return (
     <BrowserRouter>
@@ -18,6 +19,7 @@ export default function App() {
         <Route path='/projects' element={<Projects /> }></Route>
         <Route path='/dashboard' element={<Dashboard />}></Route>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
